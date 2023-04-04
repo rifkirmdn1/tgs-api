d38c492dd9186b516e79c93a4983d1a98208d6df
rifkirmdn1 <rmrifkir84@gmail.com>
2023-04-04 07:26:10 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
